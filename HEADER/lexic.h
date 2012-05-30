18b3d34caf264df405247203c72b39b1bcd7b61f
Norton Jenkins <nate@Phoenix.(none)>
2012-05-30 18:43:02 -0400

    Add langdefine comments and file ordering

--- HEADER/lexic.h
+++ HEADER/lexic.h
@@ -7,8 +7,45 @@ using namespace std;
 
 string *fromStrings=new string[10000];//C++ members
 string *toStrings=new string[10000]; // Custom language members
+void splitstr(string &c,string s);
+int linec(string);
 int members=0;                     //  The number of members
 
+/**
+*Orders the langdefines by line length.
+*This prevents the replacing of smaller words in
+*larger ones.
+*@param filepath - The path to the file
+*/
+void orderFile(string filepath)
+{
+    ifstream file(filepath.c_str());
+    int nlines=linec(filepath);
+
+    string lines[nlines];//Contains all of the lines
+
+    for(int index=0; index<nlines; index++) //Make container
+        getline(file,lines[index]);
+
+    for(int index=0; index<nlines-1; index++) //Iterate through positions
+    {
+        while(lines[index].length()<lines[index+1].length())
+        {
+            string buffer;//Loop executes while a string below it needs replacing.
+            buffer=lines[index];
+
+            lines[index]=lines[index+1];
+            lines[index+1]=buffer;
+            cout << "Let's see something."<<endl;
+            index--;
+        }
+    }
+    file.close();
+    ofstream writer(filepath.c_str());
+    for(int index=0; index<nlines; index++)
+        writer<<lines[index]<<endl;//Write the ordered awway
+    writer.close();
+}
 /**
  * Loads a language into the arrays.
  * The syntax of the file should be old_new
@@ -18,31 +55,37 @@ int members=0;                     //  The number of members
  */
 void loadLanguage(string filename,int lines)
 {
-	string buffer; //Used for parsing individual lines
-	members=lines;// How many entries the langdefines has
+    string buffer; //Used for parsing individual lines
+    members=lines;// How many entries the langdefines has
     int until=0; /*Until is used to determine how many NO_USE characters to use.
                     NO_USE characters show an unwritten line.*/
 
-	ifstream file(filename.c_str());
-	for(int index=0; index<10000; index++)
-	{
-	getline(file,buffer);
-
-	if(buffer.find("_")!=-1){
-	toStrings[index]=buffer.substr(0,buffer.find("_"));
-
-	fromStrings[index]=buffer.substr(buffer.find("_")+1,buffer.length());
-   }else{
-   until=10000-index;//Second bug
-   break;
-    }
-
-    for(int index=10000-until; index<10000; index++)
+    ifstream file(filename.c_str());
+    for(int index=0; index<10000; index++)
     {
-       toStrings[index]="/++10x";
-       fromStrings[index]="/++10x";
+        getline(file,buffer);
+        if(buffer.find("$")!=-1)
+            splitstr(buffer,"$");
+
+        if(buffer.find("_")!=-1)
+        {
+            toStrings[index]=buffer.substr(0,buffer.find("_"));
+
+            fromStrings[index]=buffer.substr(buffer.find("_")+1,buffer.length());
+        }
+        else
+        {
+            until=10000-index;//Second bug
+            break;
+        }
+
+        for(int index=10000-until; index<10000; index++)
+        {
+            //\++10x is used to indicate a line not being used.
+            toStrings[index]="/++10x";
+            fromStrings[index]="/++10x";
+        }
     }
-}
 }
 /**
 *Finds the number of lines in a file.
@@ -55,7 +98,7 @@ int linec(string filename)
     int lines=0;
     string buffer;
 
-    for(;getline(file,buffer);)
+    for(; getline(file,buffer);)
     {
         lines++;
     }
@@ -67,9 +110,11 @@ int linec(string filename)
 *@param from- The string to find and replace.
 *@param to  - What to replace the string with
 */
-void replaceAll(std::string& str,  std::string& from,  std::string& to) {
+void replaceAll(std::string& str,  std::string& from,  std::string& to)
+{
     size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+    while((start_pos = str.find(from, start_pos)) != std::string::npos)
+    {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length();
     }
@@ -81,32 +126,35 @@ void replaceAll(std::string& str,  std::string& from,  std::string& to) {
 *The translator works on an explicit basis. Every string meeting its criteria will be
 *removed. This allows sensitive strings to leave before translation begins. You must place
 *the sensitive string in between the two magic characters.
+*Splitstr also works on langdefines files.
 *@param toSplit   - The string to remove sensitive strings from.
 *@param magicahar - The character used in the code file to denote a sensitive string. "$" is the standard.
 */
-void splitstr(string &toSplit, string magichar){
-if(toSplit.find(magichar)!=-1){
-
-    stringstream buffer;
-
-    string copy=toSplit;
-    string cnholder="";
-    string nope="";
-
-
+void splitstr(string &toSplit, string magichar)
+{
     if(toSplit.find(magichar)!=-1)
-    buffer<<copy.substr(0,toSplit.find(magichar));//Before first magichar
+    {
 
-    cnholder=buffer.str();
+        stringstream buffer;
 
-    if(copy.find(magichar)!=-1)
-    copy=copy.substr(toSplit.find(magichar)+1,toSplit.length());
+        string copy=toSplit;
+        string cnholder="";
+        string nope="";
 
-    if(copy.find(magichar)!=-1)
-    buffer<<copy.substr(copy.find(magichar)+1,copy.length());//Substring of one more than the next magichar and the length.
 
-    toSplit=buffer.str();
-}
+        if(toSplit.find(magichar)!=-1)
+            buffer<<copy.substr(0,toSplit.find(magichar));//Before first magichar
+
+        cnholder=buffer.str();
+
+        if(copy.find(magichar)!=-1)
+            copy=copy.substr(toSplit.find(magichar)+1,toSplit.length());
+
+        if(copy.find(magichar)!=-1)
+            buffer<<copy.substr(copy.find(magichar)+1,copy.length());//Substring of one more than the next magichar and the length.
+
+        toSplit=buffer.str();
+    }
 }
 /**
  * Creates a file containing the old text as defined by
@@ -119,34 +167,37 @@ if(toSplit.find(magichar)!=-1){
                     one, use something gibberish. Otherwise, use your magichar. For
                     magichar documentation, see splitstr
  */
-void compile(string fileName,int lines,string toWriteTo,string magichar){
+void compile(string fileName,int lines,string toWriteTo,string magichar)
+{
 
     string line[lines];
     ifstream reader(fileName.c_str());
 
-    for(int index=0; index<lines; index++){
+    for(int index=0; index<lines; index++)
+    {
         getline(reader, line[index]);
     }
 
     for(int index=0; index<lines; index++) // Loops through each line
     {
-		for(int secInd=0; secInd<members; secInd++)//Performs replacement operations
-		{
-		    splitstr(line[index],magichar); // splitstr removes protected strings from lines[index] IF they exist.
+        for(int secInd=0; secInd<members; secInd++)//Performs replacement operations
+        {
+            splitstr(line[index],magichar); // splitstr removes protected strings from lines[index] IF they exist.
 
-		    if(fromStrings[secInd]!="/++10x"&&toStrings[secInd]!="/++10x"){
-			replaceAll(line[index],fromStrings[secInd],toStrings[secInd]);
-		}
+            if(fromStrings[secInd]!="/++10x"&&toStrings[secInd]!="/++10x")
+            {
+                replaceAll(line[index],fromStrings[secInd],toStrings[secInd]);
+            }
 
+        }
+        cout << line[index]<<endl;
+        reader.close();
     }
-    cout << line[index]<<endl;
-    reader.close();
-}
-ofstream writer(toWriteTo.c_str());
+    ofstream writer(toWriteTo.c_str());
 
-for(int index=0; index<lines; index++)//Iterate through the changed lines and write them to a file.
-{
-writer << line[index]<<endl;
-}
-writer.close();
+    for(int index=0; index<lines; index++)//Iterate through the changed lines and write them to a file.
+    {
+        writer << line[index]<<endl;
+    }
+    writer.close();
 }
--- main.cxx
+++ main.cxx
@@ -17,10 +17,10 @@ using namespace std;
 
 int main(int argc, char**argv)
 {
-
-    string argv2[5];
+    string argv2[6];
     bool interactive=false;
-    if(argc<2)
+
+    if(argc<4)
         interactive=true;
     else
         for(int index=1; index<5; index++)
@@ -45,7 +45,13 @@ int main(int argc, char**argv)
         cout << "What is the magichar? If you do not know, type \"nothing\""<<endl<<"$ ";
         cin  >> argv2[3];
         cout << endl;
+
+        cout << "Would you like the langdefines automatically ordered? y/m "<<endl<<"$ ";
+        cin  >> argv2[4];
+        cout << endl;
     }
+    if(argv2[4].find("y")!=-1)
+        orderFile(argv2[0]);
 
     loadLanguage(argv2[0],linec(argv2[0]));
     compile(argv2[1],linec(argv2[1]),argv2[2],argv2[3]);
